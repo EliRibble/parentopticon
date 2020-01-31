ff63746b55ef19a9c2ec0c53ce1d3da7f326ae00
Eli Ribble <eli.ribble@gmail.com>
2020-01-31 11:25:17 -0700

    Update denied message to show a reason.
    And play a bit with adding an image, but I never got that to work :(

--- parentopticon/ui.py
+++ parentopticon/ui.py
@@ -1,28 +1,41 @@
+import os
 import tkinter as tk
+
+ROOT = os.path.dirname(os.path.dirname(__file__))
 
 class NoApplication(tk.Frame):
 	"""An application to tell the client 'no'"""
-	def __init__(self, master=None):
+	def __init__(self, program_name, reason, master=None):
 		super().__init__(master)
 		self.master = master
+		self.program_name = program_name
+		self.reason = reason
 		self.pack()
 		self.create_widgets()
 		
 	def create_widgets(self):
-		self.ack_button = tk.Button(self)
-		self.ack_button["text"] = "Awwwww."
-		self.ack_button["command"] = self.say_hi
-		self.ack_button.pack(side="top")
+		image_path = os.path.join(ROOT, "images", "sad-dragon.gif")
+		image = tk.PhotoImage(file=image_path)
+
+		content = ("Sorry, you can't play {}.\n"
+			"Looks like it's because {}").format(
+				self.program_name,
+				self.reason)
+		self.message = tk.Label(self, text=content)
+		# self.message = tk.Label(self, image=image)
+		# self.message["image"] = image
+		self.message.pack(side="top")
 		
-		self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
-		self.quit.pack(side="bottom")
+		self.ack_button = tk.Button(self, text="Awwww.", command=self.master.destroy)
+		self.ack_button.pack(side="bottom")
 		
-	def say_hi(self):
-		print("hi there!")
 		
-def show_denied(name: str) -> None:
+def show_denied(program_name: str, reason: str) -> None:
 	"""Tell the user they can't open their program."""
-	root = tk.Tk()
-	app = NoApplication(master=root)
+	root = tk.Tk(className="parentopticon")
+	app = NoApplication(
+		master=root,
+		program_name=program_name,
+		reason=reason)
 	app.mainloop()
 
