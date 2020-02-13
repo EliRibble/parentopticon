14698c9e843a8cf97ce8cd61635716ac4a8ae925
Eli Ribble <eli.ribble@gmail.com>
2020-02-12 18:50:28 -0700

    Add interface for defining and storing windows.

--- parentopticon/db.py
+++ parentopticon/db.py
@@ -6,7 +6,32 @@ import typing
 LOGGER = logging.getLogger(__name__)
 
 Limit = collections.namedtuple("Limit", ("id", "name", "daily", "weekly", "monthly"))
+WindowWeek = collections.namedtuple("Window", (
+	"id",
+	"name",
+	"monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"))
 
+class WindowWeekDaySpan:
+	def __init__(self, id_: int, day: int, end: int, start: int, window_id: int):
+		self.id = id_
+		self.day = day
+		self.end = end
+		self.start = start
+		self.window_id = window_id
+
+	@property
+	def value(self) -> str:
+		return "{}-{}".format(self.start, self.end)
+
+class WindowWeekDay:
+	def __init__(self, window_id: int, day: int, spans: typing.List[WindowWeekDaySpan]):
+		self.window_id = window_id
+		self.day = day
+		self.spans = sorted(spans, key=lambda s: s.start)
+
+	@property
+	def value(self) -> str:
+		return ",".join(span.value for span in self.spans)
 
 class Connection:
 	"Class that encapsulates all the interface to the DB."
@@ -28,7 +53,7 @@ class Connection:
 
 	def limit_get(self, limit_id: int) -> Limit:
 		self.cursor.execute(
-			"SELECT id, name, daily, weekly, monthly FROM ProgramGroupLimit WHERE rowid = ?", (limit_id,))
+			"SELECT id, name, daily, weekly, monthly FROM ProgramGroupLimit WHERE id = ?", (limit_id,))
 		data = self.cursor.fetchone()
 		return Limit(
 			id = data[0],
@@ -48,6 +73,60 @@ class Connection:
 		) for data in self.cursor.execute(
 			"SELECT id, name, daily, weekly, monthly FROM ProgramGroupLimit")]
 
+	def window_week_create(self, name: str) -> int:
+		self.cursor.execute(
+			"INSERT INTO WindowWeek (name) VALUES (?)",
+			(name,))
+		self.connection.commit()
+		return self.cursor.lastrowid
+
+	def window_week_get(self, window_id: int) -> WindowWeek:
+		monday = self.window_week_day_get(window_id, 0)
+		tuesday = self.window_week_day_get(window_id, 1)
+		wednesday = self.window_week_day_get(window_id, 2)
+		thursday = self.window_week_day_get(window_id, 3)
+		friday = self.window_week_day_get(window_id, 4)
+		saturday = self.window_week_day_get(window_id, 5)
+		sunday = self.window_week_day_get(window_id, 6)
+		self.cursor.execute(
+			"SELECT id, name FROM WindowWeek WHERE id = ?", (window_id,))
+		data = self.cursor.fetchone()
+		return WindowWeek(
+			id = data[0],
+			name = data[1],
+			monday = monday,
+			tuesday = tuesday,
+			wednesday = wednesday,
+			thursday = thursday,
+			friday = friday,
+			saturday = saturday,
+			sunday = sunday,
+		)
+
+	def window_week_day_span_create(self, day: int, end: int, start: int, window_id: int) -> int:
+		self.cursor.execute(
+			"INSERT INTO WindowWeekDaySpan (day, end, start, window_id) VALUES (?, ?, ?, ?)",
+			(day, end, start, window_id))
+		self.connection.commit()
+		return self.cursor.lastrowid
+
+	def window_week_day_get(self, window_id: int, day: int) -> WindowWeekDay:
+		spans = [WindowWeekDaySpan(
+			id_ = data[0],
+			day = data[1],
+			end = data[2],
+			start = data[3],
+			window_id = data[4],
+		) for data in self.cursor.execute(
+			"SELECT id, day, end, start, window_id FROM WindowWeekDaySpan WHERE window_id = ? AND day = ?",
+			(window_id, day),
+		)]
+		return WindowWeekDay(
+			day = day,
+			spans = spans,
+			window_id = window_id,
+		)
+
 	def _create_tables(self):
 		LOGGER.info("Ensuring DB tables exist.")
 		self.cursor.execute(
@@ -62,19 +141,6 @@ class Connection:
 				daily INTEGER NOT NULL,
 				weekly INTEGER NOT NULL,
 				monthly INTEGER NOT NULL
-			);""")
-		self.cursor.execute(
-			"""CREATE TABLE IF NOT EXISTS WindowWeek (
-				id INTEGER PRIMARY KEY AUTOINCREMENT,
-				name TEXT NOT NULL
-			);""")
-		self.cursor.execute(
-			"""CREATE TABLE IF NOT EXISTS WindowWeekDayWindow (
-				id INTEGER PRIMARY KEY AUTOINCREMENT,
-				day INTEGER NOT NULL,
-				end TEXT NOT NULL,
-				start TEXT NOT NULL,
-				windowweek INTEGER -- WindowWeek fk
 			);""")
 		self.cursor.execute(
 			"""CREATE TABLE IF NOT EXISTS ProgramGroup (
@@ -106,6 +172,19 @@ class Connection:
 				period INT NOT NULL
 			);""")
 		self.cursor.execute(
+			"""CREATE TABLE IF NOT EXISTS WindowWeek (
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				name TEXT NOT NULL
+			);""")
+		self.cursor.execute(
+			"""CREATE TABLE IF NOT EXISTS WindowWeekDaySpan (
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				day INTEGER NOT NULL,
+				end INTEGER NOT NULL,
+				start INTEGER NOT NULL,
+				window_id INTEGER -- WindowWeek fk
+			);""")
+		self.cursor.execute(
 			"""CREATE TABLE IF NOT EXISTS WindowWeekDayOverride (
 				id INTEGER PRIMARY KEY AUTOINCREMENT,
 				created TEXT NOT NULL,
@@ -114,25 +193,3 @@ class Connection:
 				message TEXT,
 				windowset TEXT NOT NULL
 			);""")
-
-# Tables
-#  UserSession
-#   end: datetime
-#   start: datetime
-#  ProgramSession
-#   end: datetime
-#   start: datetime
-#   program: str
-#  LimitBonus
-#   amount: int
-#   created: datetime
-#   creator: str
-#   effective: datetime
-#   message: str
-#   period: enum(day, week, month)
-#  WindowWeekDayOverride
-#   created: datetime
-#   creator: str
-#   effective: date
-#   message: str
-#   windowset: str
