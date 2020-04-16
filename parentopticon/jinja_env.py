d0cd4116c462db4391e20ad58c75b9de5a38feeb
Eli Ribble <eli.ribble@gmail.com>
2020-04-16 23:46:41 +0000

    Re-add the timespan filter

--- parentopticon/jinja_env.py
+++ parentopticon/jinja_env.py
@@ -6,10 +6,29 @@ import jinja2
 def _humanize(t: datetime.datetime) -> str:
 	return arrow.get(t).humanize() if t else "none"
 
+def _timespan(t: datetime.timedelta) -> str:
+	s = t.total_seconds()
+	if s < 10:
+		return "few seconds"
+	elif s < 100:
+		return "{} seconds".format(s)
+	elif s < 1000:
+		return "few minutes"
+	elif s < (60 * 60 * 100):
+		return "{} minutes".format(s / 60)
+	elif s < (60 * 60 * 30):
+		return "{} hours".format(s / (60 * 60))
+	elif s < (60 * 60 * 24 * 400):
+		return "{} days".format(s / (60 * 60 * 24))
+	else:
+		return "{} years".format(s / (60 * 60 * 24 * 365))
+
+
 def create() -> jinja2.Environment:
 	env = jinja2.Environment(
 		loader=jinja2.PackageLoader("parentopticon", "templates"),
 		autoescape=jinja2.select_autoescape(["html", "xml"]),
 	)
 	env.filters["humanize"] = _humanize
+	env.filters["timespan"] = _timespan
 	return env
