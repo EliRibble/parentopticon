2a14f5286d159bf7ff47872932d91bc459207141
Eli Ribble <eli.ribble@gmail.com>
2020-05-20 07:37:22 -0600

    Add ability to cancel requests so they don't work.
    Eventually we'll move this to redirecting to an error page.

--- browser-extension/background.js
+++ browser-extension/background.js
@@ -1,22 +1,31 @@
 function listener(details) {
-  fetch("http://odroid.lan/website", {
-    method: "POST",
-	headers: {
-	  "Content-Type": "application/json",
-    },
-	body: JSON.stringify({
-	  documentUrl: details.documentUrl,
-	  incognito: details.incognito,
-	  originUrl: details.originUrl,
-	  url: details.url,
-	}),
-  })
-  .then(response => {
-    console.log("Response " + response.status);
+  return new Promise((resolve, reject) => {
+    fetch("http://odroid.lan/website", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        documentUrl: details.documentUrl,
+        incognito: details.incognito,
+        originUrl: details.originUrl,
+        url: details.url,
+      }),
+    })
+    .then(response => {
+      if(response.status == 204) {
+        resolve();
+      }
+      resolve({cancel: true});
+    }).catch(error => {
+      console.error("Failed to talk to parentopticon", error);
+      resolve({cancel: true});
+    });
   });
 }
 
 browser.webRequest.onBeforeRequest.addListener(
   listener,
-  {urls: ["<all_urls>"], types: ["main_frame"]}
+  {urls: ["<all_urls>"], types: ["main_frame"]},
+  ["blocking"],
 );
